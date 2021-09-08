46027c944d26605270ba4a70acc3b0504c17d4d8
evelynsba <evelynsba@gmail.com>
2021-09-08 15:26:07 -0300

    alteracao da funcao do botao de inicio

--- js/firstpage.js
+++ js/firstpage.js
@@ -2,24 +2,7 @@
 const botao = document.getElementById('button');
 
 
-//evitar que a pagina atuaize 
-// form.addEventListener("submit", function(event){
-//     event.preventDefault();
-// })
 
-botao.addEventListener("click", function(){
-    if(document.getElementById('button').clicked == true)
-{
-   alert("button was clicked");
+function abrirNovaAba(){
+    open("file:///C:/Users/Dell%20E7450%20i5/Desktop/resilia/projeto1/secondscreen.html")
 }
-else{
-    open("./src/secondscreen.html")
-}
-});
-
-
-
-
-// function abrirNovaAba(){
-//     open("file:///C:/Users/Dell%20E7450%20i5/Desktop/resilia/projeto1/secondscreen.html")
-// }
